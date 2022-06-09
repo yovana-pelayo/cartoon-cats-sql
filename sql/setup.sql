fc1cf5cf7cd1e448a44857de15a59302a7db9a33
Yovana Pelayo <yovana_pelayo@yahoo.com>
2022-06-09 11:47:42 -0700

    created a cat table and data is displaying on beekeeper

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,13 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP table if exists cat;
+
+CREATE table cat (
+    id BIGINT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR NOT NULL,
+    age INT NOT NULL
+);
+INSERT INTO cat (name, age) VALUES ('Felix', 4),
+('Tom', 100),
+('Puss in Boots', 30)
+;
