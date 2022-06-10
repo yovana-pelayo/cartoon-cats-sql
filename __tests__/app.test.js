59e056d7847bd980700ac6bed6fa344382da1af1
Yovana Pelayo <yovana_pelayo@yahoo.com>
2022-06-10 12:07:02 -0700

    passing test. set the rest of the classes in the model and test passed

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,23 +3,23 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-const { cats } = require('../data/cats');
+const { cat } = require('../data/cat');
 
-describe('cats routes', () => {
+describe('cat routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
 
-  it('/cats should return a list of cats', async () => {
-    const res = await request(app).get('/cats');
-    const expected = cats.map((cat) => {
-      return { id: cat.id, name: cat.name };
+  it('/cats should return a list of cat', async () => {
+    const res = await request(app).get('/cat');
+    const expected = cat.map((object) => {
+      return object;
     });
     expect(res.body).toEqual(expected);
   });
 
-  it('/cats/:id should return cat detail', async () => {
-    const res = await request(app).get('/cats/1');
+  it('/cat/:id should return cat detail', async () => {
+    const res = await request(app).get('/cat/1');
     const felix = {
       id: '1',
       name: 'Felix',
@@ -27,11 +27,11 @@ describe('cats routes', () => {
       url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Felix_the_cat.svg/200px-Felix_the_cat.svg.png',
       year: 1892,
       lives: 3,
-      isSidekick: false,
+      is_sidekick: false,
     };
     expect(res.body).toEqual(felix);
   });
-  -cats;
+  -cat;
 
   afterAll(() => {
     pool.end();
